74994d97f1f61581e9fd40555800cb42855753ee
abhishekh011 <144904668+abhishekh011@users.noreply.github.com>
2024-04-09 17:09:48 +0530

    app.js Corrected

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import yogaRouter from "./routers/yoga.router.js";
-const app = express();
-mongoose.connect("mongodb://localhost:27017/ayurveda")
-.then(result=>{
-   app.use(bodyParser.json());
-   app.use(bodyParser.urlencoded({extended: true}));
-
-   app.use("/yoga",yogaRouter);
-  
-   app.listen(3000,()=>{
-    console.log("Server started....");
-   })
-}).catch(err=>{
-
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import ProductRouter from "./routers/product.router.js"
+import yogaRouter from "./routers/yoga.router.js";
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/ayurveda")
@@ -29,7 +12,7 @@ mongoose.connect("mongodb://localhost:27017/ayurveda")
     app.use(bodyParser.urlencoded({extended:true}));
     
     app.use("/product",ProductRouter);
-    
+    app.use("/yoga",yogaRouter);
     app.listen(3000,()=>{
         console.log("server start");
     })
