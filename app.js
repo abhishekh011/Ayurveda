12d588560d24bf1df088c71f24e2c378e6c7fc2f
Uday Mourya <136227669+uday-mourya@users.noreply.github.com>
2024-04-09 16:39:09 +0530

    api added

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-import express from "express"
-import bodyParser from "body-parser"
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import yogaRouter from "./routers/yoga.router.js";
 
- const app = express();
-app.listen(3000,()=>{
-    console.log("server Start");
+
+const app = express();
+mongoose.connect("mongodb://localhost:27017/Ayurveda")
+.then(result=>{
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({extended: true}));
+
+   app.use("/yoga",yogaRouter);
+  
+   app.listen(3000,()=>{
+    console.log("Server started....");
+   })
+}).catch(err=>{
+    console.log(err);
 })
-
-//aa gya
-//fir aaya
-
-// const app = express();
-// app.listen(3000,()=>{
-//     console.log("server Start");
-// })
-
-// const app = express();
-// app.listen(3000,()=>{
-//     console.log("server Start");
-// })
-
