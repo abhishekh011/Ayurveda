318e1bdb618d314b2be701c3fc190b04b206b54c
kapil Dharwal <kapildharwal4@gmail.com>
2024-04-22 10:19:21 +0530

    'ssda'

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import ProductRouter from "./routers/product.router.js"
+import ProductRouter from "./routers/product.router.js";
 import yogaRouter from "./routers/yoga.router.js";
+import UserRouter from "./routers/user.router.js";
 
 const app = express();
-mongoose.connect("mongodb://localhost:27017/ayurveda")
-.then(result=>{
-
+mongoose
+  .connect("mongodb://0.0.0.0:27017/ayurveda")
+  .then((result) => {
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
-    
-    app.use("/product",ProductRouter);
-    app.use("/yoga",yogaRouter);
-    app.listen(3000,()=>{
-        console.log("server start");
-    })
-})
-.catch(err=>{
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use("/product", ProductRouter);
+    app.use("/user", UserRouter);
+    app.use("/yoga", yogaRouter);
+    app.listen(3000, () => {
+      console.log("server start");
+    });
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
